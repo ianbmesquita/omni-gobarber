1240ae0f09d230ebf067de1393708a5a50ee7d0f
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-09-15 13:53:40 -0300

    Refatoring SignIn page by create Button and Input components

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -13,11 +16,16 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input type="text" placeholder="E-mail" />
+        <Input type="text" name="email" icon={FiMail} placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
+        <Input
+          type="password"
+          name="password"
+          icon={FiLock}
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot-password">Esqueci minha senha</a>
       </form>
