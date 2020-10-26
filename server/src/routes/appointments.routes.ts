1d56f7131e2864eb4c7eea599f028256359072b2
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-10-25 21:01:35 -0300

    Creating route to get all appointments.

--- server/src/routes/appointments.routes.ts
+++ server/src/routes/appointments.routes.ts
@@ -6,6 +6,12 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.findAll();
+
+  return response.json(appointments);
+});
+
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
@@ -23,7 +29,7 @@ appointmentsRouter.post('/', (request, response) => {
 
   const appointment = appointmentsRepository.create(provider, parsedDate);
 
-  return response.json(appointment);
+  return response.status(201).json(appointment);
 });
 
 export default appointmentsRouter;
