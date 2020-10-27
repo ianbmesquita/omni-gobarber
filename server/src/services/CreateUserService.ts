a65210f97101af9269e9f440c72ed45790ab17d4
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-10-27 07:03:41 -0300

    Encrypting the user's password with bcryptjs.

--- server/src/services/CreateUserService.ts
+++ server/src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 
 import User from '../models/User';
 
@@ -20,13 +21,17 @@ class CreateUserService {
       throw new Error('Email already exists.');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await userRepository.save(user);
+
+    delete user.password;
 
     return user;
   }
