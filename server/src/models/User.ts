d59eee8613fb430aca2d672c16f6e91012e8f52d
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-10-27 04:23:15 -0300

    Creating the user model and the migration that creates the user table in the database.

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,21 +1,25 @@
+import { uuid } from 'uuidv4';
 import {
   Column,
-  CreateDateColumn,
-  Entity,
   PrimaryGeneratedColumn,
+  Entity,
+  CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
@@ -24,4 +28,4 @@ class Appointment {
   updated_at: Date;
 }
 
-export default Appointment;
+export default User;
