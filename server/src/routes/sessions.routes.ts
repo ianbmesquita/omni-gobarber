b19c7dc9b16dd0fff9fdd2c6ec5abe9bf744302c
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-10-31 02:55:12 -0300

    Creating central point for handling application errors.

--- server/src/routes/sessions.routes.ts
+++ server/src/routes/sessions.routes.ts
@@ -7,22 +7,18 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    const authenticatedUser = UserMap.toDTO(user);
+  const authenticatedUser = UserMap.toDTO(user);
 
-    return response.status(201).json({ authenticatedUser, token });
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.status(201).json({ authenticatedUser, token });
 });
 
 export default sessionsRouter;
