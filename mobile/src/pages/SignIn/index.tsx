b90a2ad281be2eaa1ad214ac003ad59459ad0745
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-11-03 16:39:12 -0300

    Creating SignIn page view.

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import logoImg from '../../assets/images/logo.png';
 
-const SignIn: React.FC = () => <Container />;
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
+};
 
 export default SignIn;
