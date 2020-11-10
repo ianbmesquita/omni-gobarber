01113afe5e540b02b26840901eea65780a68aac1
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-11-10 19:45:06 -0300

    Creating tests for create an user.

---
+++ server/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,42 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+
+import MockUsersRepository from '../repositories/mocks/MockUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreatUser', () => {
+  it('should be able to create a new user.', async () => {
+    const mockUsersRepository = new MockUsersRepository();
+
+    const createUser = new CreateUserService(mockUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'teste',
+      email: 'teste@mail.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create two users with same email.', async () => {
+    const mockUsersRepository = new MockUsersRepository();
+
+    const createUser = new CreateUserService(mockUsersRepository);
+
+    await createUser.execute({
+      name: 'teste 1',
+      email: 'teste@mail.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'teste 2',
+        email: 'teste@mail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
