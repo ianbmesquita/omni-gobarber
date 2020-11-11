2ce521065472ac8b47de47669098196b73dd1ecd
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-11-11 18:27:07 -0300

    Creating tests to send forgot password email.

--- server/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ server/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -4,19 +4,29 @@ import AppError from '@shared/errors/AppError';
 
 import MockMailProvider from '@shared/container/providers/MailProvider/mocks/MockMailProvider';
 import MockUsersRepository from '../repositories/mocks/MockUsersRepository';
+import MockUserTokensRepository from '../repositories/mocks/MockUserTokensRepository';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
 
+let mockUsersRepository: MockUsersRepository;
+let mockUserTokensRepository: MockUserTokensRepository;
+let mockMailProvider: MockMailProvider;
+let sendForgotPasswordEmail: SendForgotPasswordEmailService;
+
 describe('SendForgotPasswordEmail', () => {
-  it('should be able to recover password using email.', async () => {
-    const mockUsersRepository = new MockUsersRepository();
-    const mockMailProvider = new MockMailProvider();
+  beforeEach(() => {
+    mockUsersRepository = new MockUsersRepository();
+    mockMailProvider = new MockMailProvider();
+    mockUserTokensRepository = new MockUserTokensRepository();
 
-    const sendMail = jest.spyOn(mockMailProvider, 'sendMail');
-
-    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+    sendForgotPasswordEmail = new SendForgotPasswordEmailService(
       mockUsersRepository,
       mockMailProvider,
+      mockUserTokensRepository,
     );
+  });
+
+  it('should be able to recover password using email.', async () => {
+    const sendMail = jest.spyOn(mockMailProvider, 'sendMail');
 
     await mockUsersRepository.create({
       name: 'Ian Mesquita',
@@ -32,18 +42,26 @@ describe('SendForgotPasswordEmail', () => {
   });
 
   it('should not be able to recover a non-existing user password', async () => {
-    const mockUsersRepository = new MockUsersRepository();
-    const mockMailProvider = new MockMailProvider();
-
-    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
-      mockUsersRepository,
-      mockMailProvider,
-    );
-
     await expect(
       sendForgotPasswordEmail.execute({
         email: 'ian@mail.com',
       }),
     ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to generate a forgot password token', async () => {
+    const generateToken = jest.spyOn(mockUserTokensRepository, 'generate');
+
+    const user = await mockUsersRepository.create({
+      name: 'Ian Mesquita',
+      email: 'ian@mail.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'ian@mail.com',
+    });
+
+    expect(generateToken).toHaveBeenCalledWith(user.id);
   });
 });
