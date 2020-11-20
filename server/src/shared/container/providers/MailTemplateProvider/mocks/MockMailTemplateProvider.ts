b0a3114ae81b39033979501fcf707c27a0a6d1ac
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-11-20 16:29:48 -0300

    Applying template engining to password recovery email.

---
+++ server/src/shared/container/providers/MailTemplateProvider/mocks/MockMailTemplateProvider.ts
@@ -0,0 +1,9 @@
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+
+class MockMailTemplateProvider implements IMailTemplateProvider {
+  public async parse(): Promise<string> {
+    return 'Mail content';
+  }
+}
+
+export default MockMailTemplateProvider;
--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -24,6 +24,9 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
+  // eslint-disable-next-line no-console
+  console.error(err);
+
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error.',
