8769c45abc82bec68df6196cecb3e23d9597529a
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-11-27 10:47:27 -0300

    Creating service to list days in month available to appointments.

--- server/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ server/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,8 +1,12 @@
-import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import Appointment from '../infra/typeorm/entities/Appointment';
 
-import ICreateAppointmentsDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import ICreateAppointmentsDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentsDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
 }
