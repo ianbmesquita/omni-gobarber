2c392cee1526d2701ea6ae4ba541d25cc3c08f36
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-12-12 02:16:18 -0300

    Creating presentation of the list of scheduled appointments for the provider.

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiClock, FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
@@ -13,10 +13,14 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -63,6 +67,62 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/29241472?s=460&u=1f7adc75ce252f58f35f83f7fabbd9047cdd2a00&v=4"
+                  alt="Ian Mesquita"
+                />
+
+                <strong>Ian Mesquita</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/29241472?s=460&u=1f7adc75ce252f58f35f83f7fabbd9047cdd2a00&v=4"
+                  alt="Ian Mesquita"
+                />
+
+                <strong>Ian Mesquita</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                15:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/29241472?s=460&u=1f7adc75ce252f58f35f83f7fabbd9047cdd2a00&v=4"
+                  alt="Ian Mesquita"
+                />
+
+                <strong>Ian Mesquita</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
