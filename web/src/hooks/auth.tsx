6a1a8a6383a921c0baec3533ba6ddc8fe6ee414d
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-12-14 16:15:59 -0300

    Creating the image upload functionality for the profile.

--- web/src/hooks/auth.tsx
+++ web/src/hooks/auth.tsx
@@ -1,6 +1,5 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 
-import { userInfo } from 'os';
 import api from '../services/api';
 
 interface User {
@@ -24,6 +23,7 @@ interface AuthContextData {
   user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(user: User): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -61,8 +61,22 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(
+    (user: User) => {
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
