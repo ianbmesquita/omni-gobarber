5f5af7ab1a4d065ba1a4f1359795bc097b654925
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-12-14 12:25:55 -0300

    Creating profile page.

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import { FiClock, FiPower } from 'react-icons/fi';
 
 import DayPicker, { DayModifiers } from 'react-day-picker';
@@ -42,7 +43,9 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
